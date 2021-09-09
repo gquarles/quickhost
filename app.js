a317793f6f5c3d6163ae4bb393f43be0282924a1
Griffin <gquarles@protonmail.com>
2021-09-08 20:19:16 -0400

    Add optional blocking of external uploads

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
-require('dotenv').config()
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-var serveIndex = require('serve-index');
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const expressip = require("express-ip");
+const ipaddr = require("ipaddr.js");
+var serveIndex = require("serve-index");
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-app.use('/files', serveIndex(__dirname + '/uploads'));
+app.use(expressip().getIpInfoMiddleware);
+
+app.use(express.static(__dirname + "/public"));
+app.use("/files", serveIndex(__dirname + "/uploads"));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-const upload = multer({ storage: storage })
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", function (req, res) {
+  if (process.env.EXTERNAL_UPLOADS.toString().toLowerCase() == "false") {
+    var userIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    var ipType = ipaddr.process(userIp).range().toString();
+
+    if (ipType != "loopback" && ipType != "private") {
+      res.send("External file upload is disabled.");
+    } else {
+      res.sendFile(path.join(__dirname, "/index.html"));
+    }
+  } else {
+    res.sendFile(path.join(__dirname, "/index.html"));
+  }
 });
 
-app.get('/files/:name', function (req, res) {
+app.get("/files/:name", function (req, res) {
   let fileName = req.params.name;
 
   res.download(path.join(__dirname, `/uploads/${fileName}`));
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   return res.status(200).send(req.file);
 });
 
